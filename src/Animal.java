42e8829db9fd2210c142b1e50d8c62e638da2e8a
Ankit Khadka <khadkaankit85@gmail.com>
2024-01-23 10:59:51 -0500

    Animal OOP

---
+++ src/Animal.java
@@ -0,0 +1,37 @@
+
+class Pet{
+
+//    creating varibles for this class
+    private String name;
+    private int age;
+    private int legs;
+    private String color;
+//    this is similar to __def__ in python and is called constructor.
+    public Pet(String name,int age,int legs,String color){
+        this.name=name;
+        this.age=age;
+        this.legs=legs;
+        this.color=color;
+    }
+//    These are getter methods.
+    public String getName(){
+        return name;
+    }
+    public String getColor(){
+        return color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public int getLegs(){
+        return legs;
+    }
+}
+public class Animal {
+
+    public static void main(String[] args) {
+    Pet Dog=new Pet("Puppy",5,4,"RED");
+        System.out.println(Dog.getName());
+    }
+}
--- src/OOPInClass.java
+++ src/OOPInClass.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class OOPInClass {
+    // Scanner object should be inside a method or a constructor
+    Scanner input = new Scanner(System.in);
+
+    // Inner class should not have parameters in its declaration
+    public static class Student {
+        // Constructor for Student class
+        public Student(int rollNumber) {
+            System.out.print("Student Id is 23058");
+            System.out.println(rollNumber);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("THis is ankit khadka");
+        System.out.println("This program is running.");
+
+        // Creating an instance of the Student class
+        OOPInClass outerObject = new OOPInClass();
+        Student Angkit =new Student(59);
+//        OOPInClass.Student Angkit = outerObject.new Student(50);
     }
 }
