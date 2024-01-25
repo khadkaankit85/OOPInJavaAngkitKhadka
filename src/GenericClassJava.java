51536e5aa0fd139e8aef936384bd94cd050fd0d2
Ankit Khadka <khadkaankit85@gmail.com>
2024-01-25 10:47:09 -0500

    Generics in Java

--- src/GenericClassJava.java
+++ src/GenericClassJava.java
@@ -1,6 +1,19 @@
 public class GenericClassJava {
 
+    public static class Bird<T>{
+        T data;
+        Bird(T data){
+            this.data=data;
+        }
+        T birdDetail(){
+            return data;
+        }
+    }
     public static void main(String[] args) {
+    Bird<String> obj1=new Bird<String>("Eagle");
+//        System.out.println(obj1.birdDetail());
 
+        Bird<Integer> obj2=new Bird<Integer>(2);
+        System.out.println(obj1.birdDetail()+" has "+obj2.birdDetail()+ " legs.");
     }
 }
