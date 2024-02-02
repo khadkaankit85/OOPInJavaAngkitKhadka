2bc0ed7accb62db3314ae3a133796c60695c3105
Ankit Khadka <khadkaankit85@gmail.com>
2024-02-02 11:37:08 -0500

    creating interface

---
+++ src/AbstractMethods2.java
@@ -0,0 +1,21 @@
+public class AbstractMethods2 {
+    public static void main(String[] args) {
+        interface Cars{
+            public void tireCount();
+            public void doorCount();
+        }
+         class CarDetail implements Cars{
+            public void tireCount(){
+                System.out.println("Cars Have four tires");
+            }
+            public void doorCount(){
+                System.out.println("Cars have doors");
+            }
+        }
+
+        CarDetail carOne=new CarDetail();
+        carOne.doorCount();
+        carOne.tireCount();
+    }
+
+}
